3bd98799bc4281a0225ee32bfa52cd38d613b9c7
Nicolas Andino <nicolas.n.andino@gmail.com>
2025-10-02 14:58:53 -0300

    mejoras_carrito

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { getProductById } from "../../data/mockAPI";
 import { useEffect, useState, useContext } from "react";
 import PesoPick from "../PesoPick/PesoPick";
@@ -9,8 +9,8 @@ import cartContext from "../context/cartContext";
 function ItemDetailContainer() {
   const { idParam } = useParams();
   const [product, setProduct] = useState({ loading: true });
-  
-  const [selectedQuantity, setSelectedQuantity] = useState("1 Kilo"); 
+
+  const [selectedQuantity, setSelectedQuantity] = useState("1 Kilo");
 
   const { addToCart } = useContext(cartContext);
 
@@ -18,20 +18,29 @@ function ItemDetailContainer() {
     getProductById(idParam)
       .then(response => {
         setProduct(response);
-        setSelectedQuantity("1 Kilo"); 
+        setSelectedQuantity("1 Kilo");
       })
       .catch(error => alert(error));
   }, [idParam]);
 
   if (product.loading) {
-    return <p>Cargando producto...</p>;
+    return <p className="item-card-loading">Cargando producto...</p>;
+  }
+
+
+  let categoryPath = null;
+  let categoryName = null;
+
+  if (product && product.category) {
+    categoryPath = `/category/${product.category}`;
+    categoryName = product.category.charAt(0).toUpperCase() + product.category.slice(1);
   }
 
   const handleAddToCart = () => {
     const itemWithDetails = {
       ...product,
-      quantityLabel: selectedQuantity, 
-      baseId: product.id 
+      quantityLabel: selectedQuantity,
+      baseId: product.id
     };
 
     addToCart(itemWithDetails);
@@ -44,13 +53,30 @@ function ItemDetailContainer() {
         <img className="item-card-img-detail" src={product.img} alt={product.title} />
         <p className="item-description">{product.description}</p>
         <h3 className="item-card-price">Precio: ${product.price} / Kilo</h3>
-        
-        <PesoPick 
-          selectedQuantity={selectedQuantity} 
-          onSelect={setSelectedQuantity} 
+
+        <PesoPick
+          selectedQuantity={selectedQuantity}
+          onSelect={setSelectedQuantity}
         />
-        
+
         <ButtonAddToCart onAddToCart={handleAddToCart} />
+
+        <div className="return-buttons-wrapper">
+          {categoryPath && (
+            <Link to={categoryPath}>
+              <button className="btn-return-to-category">
+                « Volver a {categoryName}
+              </button>
+            </Link>
+          )}
+
+          <Link to="/">
+            <button className="btn-return-to-home-from-detail">
+              Ir al Menú Principal
+            </button>
+          </Link>
+        </div>
+
       </div>
     </div>
   );
