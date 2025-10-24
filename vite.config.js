2bf953e01f5d56377f4432ad7bc9e7d35b5fba5f
Nicolas Andino <nicolas.n.andino@gmail.com>
2025-10-24 17:30:43 -0300

    mejoras-finales

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  define: {
-    'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_API_KEY),
-    'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_AUTH_DOMAIN),
-    'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_PROJECT_ID),
-    'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_STORAGE_BUCKET),
-    'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
-    'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_APP_ID),
-  },
+  
 });
