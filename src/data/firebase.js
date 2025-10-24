a84d8687049025037a6b515d2392020b48d31e48
Nicolas Andino <nicolas.n.andino@gmail.com>
2025-10-23 21:30:36 -0300

    mejoras-finales

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore, collection, addDoc, getDocs, getDoc, doc, query, where } 
 import productsArray from './products';
 
 const firebaseConfig = {
-   apiKey: import.meta.env.VITE_PUBLIC_FIREBASE_API_KEY, 
-   authDomain: import.meta.env.VITE_PUBLIC_FIREBASE_AUTH_DOMAIN,
-   projectId: import.meta.env.VITE_PUBLIC_FIREBASE_PROJECT_ID,
-   storageBucket: import.meta.env.VITE_PUBLIC_FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: import.meta.env.VITE_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-   appId: import.meta.env.VITE_PUBLIC_FIREBASE_APP_ID,
+   apiKey: process.env.VITE_PUBLIC_FIREBASE_API_KEY, 
+   authDomain: process.env.VITE_PUBLIC_FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.VITE_PUBLIC_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.VITE_PUBLIC_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.VITE_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.VITE_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  
+  define: {
+    'process.env.VITE_PUBLIC_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_API_KEY),
+    'process.env.VITE_PUBLIC_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_AUTH_DOMAIN),
+    'process.env.VITE_PUBLIC_FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_PROJECT_ID),
+    'process.env.VITE_PUBLIC_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_STORAGE_BUCKET),
+    'process.env.VITE_PUBLIC_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+    'process.env.VITE_PUBLIC_FIREBASE_APP_ID': JSON.stringify(process.env.VITE_PUBLIC_FIREBASE_APP_ID),
+  },
+});
